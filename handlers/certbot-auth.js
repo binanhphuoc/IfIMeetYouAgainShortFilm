c2ce7af25f1e8423517ae3e94b4dfc3fc170f883
Phuoc Do <binanhphuoc@gmail.com>
2019-01-25 17:46:25 -0600

    static.json

--- handlers/certbot-auth.js
+++
@@ -1,9 +0,0 @@
-express = require("express");
-
-app = express();
-
-app.get("/.well-known/acme-challenge/SUQKxNB7zm52zZdMOpsuMFG3k_ViWna-kCUQRJwvx5M", (req, res) => {
-  res.send("SUQKxNB7zm52zZdMOpsuMFG3k_ViWna-kCUQRJwvx5M.9WT6oplCzS4uXrBLjNWoLLrn0vXOtFmG2ylbFypRXnM");
-});
-
-app.listen(5000);
