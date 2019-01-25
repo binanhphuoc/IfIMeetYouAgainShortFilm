a94914bb8cdcdc237aa7270c602f265e13e93133
Phuoc Do <binanhphuoc@gmail.com>
2019-01-25 16:06:47 -0600

    lint fix

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import Home from "./home/index";
 import NotFound from "./404/index";
 
 export default () => {
   return (
-    <main>
+    <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="*" component={NotFound} />
       </Switch>
-    </main>
+    </BrowserRouter>
   );
 };
